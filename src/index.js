23d849ec80775c68fcc0ea2d1849336c3cd87599
melkorAMP <melkor@aleph.engineering>
2024-11-12 22:13:28 -0300

    #1 First Commit
    - Create the Project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Home from './pages/HomePage';
 import reportWebVitals from './reportWebVitals';
+import './styles/global.css'
+import MainLayout from './layouts/MainLayout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainLayout>
+      <Home />
+    </MainLayout>
   </React.StrictMode>
 );
 
