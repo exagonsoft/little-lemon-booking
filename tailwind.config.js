ca6432898d2adc92ab8e7b7a51860a87760091d2
melkorAMP <melkor@aleph.engineering>
2024-11-12 23:14:57 -0300

    #2 Create Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#f5f1f1",
+      secondary: "#ffffff",
+      backGround: "#3f554e"
+    },
+    extend: {
+
+    },
   },
   plugins: [],
 }
